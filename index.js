64011fff71ecf99bc9ad973b2222c4f841338a84
Cesar Santos <cesarsantossouza99@gmail.com>
2022-12-09 09:37:43 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { request, response } = require("express");
 const express = require("express");
 const uuid = require('uuid')
+const cors = require ('cors')
 
 const app = express()
 const porta = 3001
 app.use(express.json())
+app.use(cors())
 
 
 const comandas = []
@@ -31,9 +33,9 @@ app.get('/comandas', (request, response) => {
 })
 
 app.post('/comandas', (request, response) => {
-    const { pedido, cliente, valor } = request.body
+    const { pedido, cliente } = request.body
 
-    const pedidos = { id: uuid.v4(), pedido, cliente, valor }
+    const pedidos = { id: uuid.v4(), pedido, cliente }
 
     comandas.push(pedidos)
 
